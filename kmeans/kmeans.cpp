cece27f1a6e075d55b94156d2b6964d256d7a2d2
2077946749 <2077946749@qq.com>
2020-06-05 14:45:56 +0800

    0

--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -1,20 +1,157 @@
-﻿// kmeans.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include<opencv2/opencv.hpp>
 #include <iostream>
+
+using namespace std;
+using namespace cv;
+
+VideoCapture createInput(bool useCamera, std::string videoPath)
+{
+	VideoCapture capVideo;
+	if (useCamera) {
+		capVideo.open(0);
+	}
+	else {
+		capVideo.open(videoPath);
+	}
+	return capVideo;
+}
+
+int createMaskByKmeans(cv::Mat src, cv::Mat & mask)
+{
+	if ((mask.type() != CV_8UC1)
+		|| (src.size() != mask.size())) {
+		return 0;
+	}
+
+	int width = src.cols;
+	int height = src.rows;
+
+	int pixNum = width * height;
+	int clusterCount = 2;
+	Mat labels;
+	Mat centers;
+
+	//制作kmeans用的数据
+	Mat sampleData = src.reshape(3, pixNum);
+	Mat km_data;
+	sampleData.convertTo(km_data, CV_32F);
+
+	//执行kmeans
+	TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 0.1);
+	kmeans(km_data, clusterCount, labels, criteria, clusterCount, KMEANS_PP_CENTERS, centers);
+
+	//制作mask
+	uchar fg[2] = { 0,255 };
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			mask.at<uchar>(row, col) = fg[labels.at<int>(row*width + col)];
+		}
+	}
+
+	return 0;
+}
+void segColor()
+{
+
+	Mat src = imread("movie.jpg");
+	if (src.empty()) {
+		cout << "fail to open" << endl;
+	}
+
+	Mat mask = Mat::zeros(src.size(), CV_8UC1);
+	createMaskByKmeans(src, mask);
+
+	imshow("src", src);
+	imshow("mask", mask);
+
+	waitKey(0);
+
+}
+
+int kMeansDemo()
+{
+	const int MAX_CLUSTERS = 5;
+	Scalar colorTab[] =
+	{
+		Scalar(0, 0, 255),
+		Scalar(0,255,0),
+		Scalar(255,100,100),
+		Scalar(255,0,255),
+		Scalar(0,255,255)
+	};
+
+	Mat img(500, 500, CV_8UC3);
+	RNG rng(12345);
+
+	for (;;)
+	{
+		int k, clusterCount = rng.uniform(2, MAX_CLUSTERS + 1);
+		int i, sampleCount = rng.uniform(1, 1001);
+		Mat points(sampleCount, 1, CV_32FC2), labels;
+
+		clusterCount = MIN(clusterCount, sampleCount);
+		std::vector<Point2f> centers;
+
+		for (k = 0; k < clusterCount; k++)
+		{
+			Point center;
+			center.x = rng.uniform(0, img.cols);
+			center.y = rng.uniform(0, img.rows);
+			Mat pointChunk = points.rowRange(k*sampleCount / clusterCount,
+				k == clusterCount - 1 ? sampleCount :
+				(k + 1)*sampleCount / clusterCount);
+			rng.fill(pointChunk, RNG::NORMAL, Scalar(center.x, center.y), Scalar(img.cols*0.05, img.rows*0.05));
+		}
+
+		randShuffle(points, 1, &rng);
+
+		double compactness = kmeans(points, clusterCount, labels,
+			TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 1.0),
+			3, KMEANS_PP_CENTERS, centers);
+
+		img = Scalar::all(0);
+
+		for (i = 0; i < sampleCount; i++)
+		{
+			int clusterIdx = labels.at<int>(i);
+			Point ipt = points.at<Point2f>(i);
+			circle(img, ipt, 2, colorTab[clusterIdx], FILLED, LINE_AA);
+		}
+		for (i = 0; i < (int)centers.size(); ++i)
+		{
+			Point2f c = centers[i];
+			circle(img, c, 40, colorTab[i], 1, LINE_AA);
+		}
+		cout << "Compactness: " << compactness << endl;
+
+		imshow("clusters", img);
+
+		char key = (char)waitKey();
+		if (key == 27 || key == 'q' || key == 'Q') // 'ESC'
+			break;
+	}
+
+	return 0;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//开始计时
+	double start = static_cast<double>(cvGetTickCount());
+
+	//segColor();
+
+	kMeansDemo();
+
+	//结束计时
+	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
+
+	//显示时间
+	cout << "processing time:" << time / 1000 << "ms" << endl;
+
+
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
